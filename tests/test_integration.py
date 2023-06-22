4197a17c3658f0235bd4f563eefd2a3f24b4c757
Alex Hill <alex.hill@gmail.com>
2023-06-22 14:30:21 +0100

    add test of restore from relative path

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,7 +5,7 @@ from src.privateer import cli
 from src.privateer.config import PrivateerConfig
 
 
-def test_backup():
+def test_backup_and_restore():
     cfg = PrivateerConfig("config")
     test = cfg.get_host("test")
     made_dir = False
@@ -13,15 +13,19 @@ def test_backup():
         os.mkdir(test.path)
         made_dir = True
     try:
+        # backup
         res = cli.main(["backup", "config", "--to=test"])
         assert res == "Backed up targets 'orderly_volume', 'another_volume' to host 'test'"
         assert os.path.isfile(os.path.join(test.path, "orderly_volume.tar"))
         assert os.path.isfile(os.path.join(test.path, "another_volume.tar"))
+        # restore
+        res = cli.main(["restore", "config", "--from=test"])
+        assert res == "Restored targets 'orderly_volume', 'another_volume' from host 'test'"
     finally:
         if made_dir:
             shutil.rmtree(test.path)
 
 
-def test_restore():
+def test_restore_no_backups():
     res = cli.main(["restore", "config", "--from=test"])
     assert res == "No valid backups found. Doing nothing."
